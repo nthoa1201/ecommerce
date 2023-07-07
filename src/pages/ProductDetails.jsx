5fb1ddd05b971f6e28f8ed214d1ed99183a332e4
Nguyen Thuan Hoa <nthoa.cv1201@gmail.com>
2023-07-07 11:56:29 +0700

    added product detail page

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,10 +1,59 @@
-import React from 'react';
+import React, {useContext} from 'react';
+
+import {useParams} from 'react-router-dom';
+
+import {CartContext} from '../contexts/CartContext.jsx';
+import {ProductContext} from '../contexts/ProductContext.jsx';
 
 const ProductDetails = () => {
+    const {id} = useParams()
+
+    const {products} = useContext(ProductContext)
+    const {addTocart} = useContext(CartContext)
+
+    const product = products.find(item => item.id === parseInt(id))
+
+    // if product is not found
+    if (!product) {
+        return (
+            <section className="h-screen flex justify-center items-center">
+                Loading...
+            </section>
+        )
+    }
+
+    console.log(product)
+    const {title, price, description, image} = product
     return (
-        <div>
-           <p>Product Details Page</p>
-        </div>
+        <section className="pt-32 pb-12 lg:py-32 h-screen flex items-center">
+            <div className="container mx-auto">
+                {/*wrapper*/}
+                <div className="flex flex-col lg:flex-row items-center">
+                    {/*img*/}
+                    <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0">
+                        <img
+                            src={image}
+                            alt=""
+                            className="max-w-[200px] lg:max-w-sm"/>
+                    </div>
+                    {/*text*/}
+                    <div className="flex-1 text-center lg:text-left">
+                        <h1 className="text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0">{title}</h1>
+                        <div className="text-xl text-red-500 font-medium mb-6">
+                            $ {price}
+                        </div>
+                        <p className="mb-8">{description}</p>
+                        <button
+                            onClick={() => addTocart(product, product.id)}
+                            className="bg-primary py-4 px-8 text-white">
+                            Add to cart
+                        </button>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+
     );
 };
 
